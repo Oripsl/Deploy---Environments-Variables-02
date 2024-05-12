d8befbae756833fbbba5f2d57fbc7408b5699951
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-05-12 14:45:30 +0200

    first commit

---
+++ src/main/java/com/orcone/ez/EzApplication.java
@@ -0,0 +1,13 @@
+package com.orcone.ez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzApplication.class, args);
+	}
+
+}
